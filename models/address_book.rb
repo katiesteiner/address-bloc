c78e40684fdfba728f1128dd1dd3550607797e0d
katiesteiner <katiewsteiner@gmail.com>
2015-07-04 21:43:31 -0400

    AddressBloc Menu Checkpoint-Not Sure It Works

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,20 +1,29 @@
-require_relative "entry.rb"
-class AddressBook
-  attr_accessor :entries
+#require_relative "entry.rb"
+require_relative "controllers/menu_controller"
 
-  def initialize
-    @entries = []
-  end
+menu = MenuController.new
 
-  def add_entry(name, phone, email)
-    index = 0
-    @entries.each do |entry|
-      if name < entry.name
-        break
-      end
-      index += 1
-    end
+system "clear"
+puts "Welcome to AddressBloc"
 
-    @entries.insert(index, Entry.new(name, phone, email))
-  end
-end
+menu.main_menu
+
+#class AddressBook
+ # attr_accessor :entries
+
+ # def initialize
+  #  @entries = []
+#  end
+
+ # def add_entry(name, phone, email)
+  #  index = 0
+   # @entries.each do |entry|
+    #  if name < entry.name
+     #   break
+     # end
+     # index += 1
+    #end
+
+   # @entries.insert(index, Entry.new(name, phone, email))
+  #end
+#end
